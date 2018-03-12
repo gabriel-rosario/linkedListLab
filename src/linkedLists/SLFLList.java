7fb4c44e991387477c69396d6687427c24fb5aaf
ICOM STUDENTS <4035b13@icaco.ece.uprm.edu>
2018-03-12 13:13:48 -0400

    part 1

--- src/linkedLists/SLFLList.java
+++ src/linkedLists/SLFLList.java
@@ -24,47 +24,89 @@ public class SLFLList<E> extends SLList<E>
 	
 	public void addFirstNode(Node<E> nuevo) {
 		// TODO Auto-generated method stub
-		
+		SNode <E> sn = (SNode<E>) nuevo;
+		sn.setNext(first);
+		first = sn;
 	}
 
 	public void addNodeAfter(Node<E> target, Node<E> nuevo) {
 		// TODO Auto-generated method stub
+		SNode<E> ts = (SNode<E>) target;
+		SNode<E> ns = (SNode<E>) nuevo;
+		
+		if(ts == last){
+			last = ns;
+		}else{
+			SNode<E> next = ts.getNext();
+			ns.setNext(next);
+		}
+		ts.setNext(ns);
+		length++;
+
 		
 	}
 
 	public void addNodeBefore(Node<E> target, Node<E> nuevo) {
 		// TODO Auto-generated method stub
+		//pre: target is valid Node in 'this'
+		//pre: nuevo es tipo SNdoe
+			SNode<E> ts = (SNode<E>) target;
+			SNode<E> ns = (SNode<E>) nuevo;
+				
+			ns.setNext(ts);
+				
+			if(ts == first){
+				first = ns;
+			}else{
+				SNode<E> prev = (SNode<E>) first;
+				while(prev!=ts){
+					prev = prev.getNext();
+				}
+				prev.setNext(ns);
+			}
+			length++;
+				
 		
 	}
 
 	public Node<E> getFirstNode() throws NoSuchElementException {
 		// TODO Auto-generated method stub
-		return null;
+		return first;
 	}
 
 	public Node<E> getLastNode() throws NoSuchElementException {
 		// TODO Auto-generated method stub
-		return null;
+		return last;
 	}
 
 	public Node<E> getNodeAfter(Node<E> target) throws NoSuchElementException {
 		// TODO Auto-generated method stub
-		return null;
+		SNode<E> ts = (SNode<E>)target;		
+		
+		return ts.getNext();
 	}
 
 	public Node<E> getNodeBefore(Node<E> target)
 			throws NoSuchElementException {
 		// TODO Auto-generated method stub
-		return null;
+		SNode ts = (SNode<E>) target;
+		SNode prev = (SNode<E>) first;
+		
+		while(prev != ts){
+			prev = prev.getNext();
+		}
+		return prev;
 	}
 
 	public int length() {
 		// TODO Auto-generated method stub
-		return 0;
+		return length;
 	}
 
 	public void removeNode(Node<E> target) {
 		// TODO Auto-generated method stub
+		SNode<E> ntr = (SNode<E>) target;
+		SNode<E> next = ntr.getNext();
 		
 	}
 	
